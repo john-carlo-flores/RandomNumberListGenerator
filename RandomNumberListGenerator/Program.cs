dd7ec84660d84631955b1dc6c64c142a5b0a0c4a
tothenextcode <tothenextcode@gmail.com>
2022-07-05 11:35:22 -0600

    Add project files.

---
+++ RandomNumberListGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RandomNumberListGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
